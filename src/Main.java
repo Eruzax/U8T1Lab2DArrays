2e38dc47abd06d2da453c2c553fa5ad49f8b1912
BT_1E10_19 <dennism27@nycstudents.net>
2022-03-02 13:44:25 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -41,5 +41,39 @@ public class Main
         //2a - c
         int[][] ints = {{0, 0, 0}, {0, 0, 0}};
         int[][] ints2 = {{0, 0}, {0, 0}, {0, 0}};
+
+        for (int[] intss : ints)
+        {
+            System.out.println(Arrays.toString(intss));
+        }
+
+        for (int[] intss : ints2)
+        {
+            System.out.println(Arrays.toString(intss));
+        }
+
+        ints[0][0] = 1;
+        ints[0][1] = 2;
+        ints[0][2] = 3;
+        ints[1][0] = 4;
+        ints[1][1] = 5;
+        ints[1][2] = 6;
+
+        ints2[0][0] = 1;
+        ints2[0][1] = 2;
+        ints2[1][0] = 3;
+        ints2[1][1] = 4;
+        ints2[2][0] = 5;
+        ints2[2][1] = 6;
+
+        for (int[] intss : ints)
+        {
+            System.out.println(Arrays.toString(intss));
+        }
+
+        for (int[] intss : ints2)
+        {
+            System.out.println(Arrays.toString(intss));
+        }
     }
 }
