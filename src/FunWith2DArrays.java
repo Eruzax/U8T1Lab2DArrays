388d59b43eee04e54cd5e2a33b5e15c396be7048
BT_1E10_19 <dennism27@nycstudents.net>
2022-03-03 13:39:15 -0500

    update

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -12,4 +12,42 @@ public class FunWith2DArrays
         System.out.println(str[str.length - 1][0]);
         System.out.println(str[str.length - 1][str[str.length - 1].length - 1]);
     }
+
+    public static double average(int[][] ints)
+    {
+        double counter = 0.0;
+        double total = 0.0;
+        for (int i = 0; i < ints.length; i++)
+        {
+            for(int j = 0; j < ints[0].length; j++)
+            {
+                total += ints[i][j];
+                counter++;
+            }
+        }
+        return total / counter;
+    }
+
+    public static int[] indexFound(String[][] str, String target)
+    {
+        int[] foundIndex = new int[2];
+
+        for (int i = 0; i < str.length; i++)
+        {
+            for (int j = 0; j < str[0].length; j++)
+            {
+                String element =str[i][j];
+                if (element.equals(target))
+                {
+                    foundIndex[0] = i;
+                    foundIndex[1] = j;
+
+                    return foundIndex;
+                }
+            }
+        }
+        foundIndex[0] = -1;
+        foundIndex[1] = -1;
+        return foundIndex;
+    }
 }
